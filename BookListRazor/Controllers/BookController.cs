c1b4dddfa68758411319a7ef0935ab8f82d3d753
ritzyRosabel <rosabelolugbenga@yahoo.com>
2020-07-06 23:17:49 +0100

    Jquery setup on index

---
+++ BookListRazor/Controllers/BookController.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using BookListRazor.Model;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BookListRazor.Controllers
+{
+    public class BookController : Controller
+    {
+        private readonly ApplicationDbContext _db;
+        public BookController(ApplicationDbContext db)
+        {
+            _db = db;
+        }
+        [HttpGet]
+        [Route("api/Books")]
+        public IActionResult GetAll()
+        {
+
+            return Json(new { data = _db.Books.ToList() });
+        }
+    }
+}
