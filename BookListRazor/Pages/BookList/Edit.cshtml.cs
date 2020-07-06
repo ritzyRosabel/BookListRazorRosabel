7b87c99b346ec3e3c1287d7891976a9377135b9c
ritzyRosabel <rosabelolugbenga@yahoo.com>
2020-07-06 21:55:13 +0100

    constructor, property, get method for edit

---
+++ BookListRazor/Pages/BookList/Edit.cshtml.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using BookListRazor.Model;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace BookListRazor.Pages.BookList
+{
+    public class EditModel : PageModel
+    {
+        private ApplicationDbContext _db;
+
+        public EditModel(ApplicationDbContext db)
+        {
+            _db = db;
+        }
+        [BindProperty]
+        public Book Book { get; set; }
+        public async Task OnGet( int id)
+        {
+            if (id != 0)
+            {
+                Book = await _db.Books.FindAsync(id);
+
+            }
+        }
+
+        public async Task  OnPost() 
+        {
+            
+        }
+
+    }
+}
