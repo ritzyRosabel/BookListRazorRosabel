d9652682b2db515c82349b5c5678f0b7c3cb57b4
ritzyRosabel <rosabelolugbenga@yahoo.com>
2020-07-06 21:06:19 +0100

    ISBN property

--- BookListRazor/Model/Book.cs
+++ BookListRazor/Model/Book.cs
@@ -13,5 +13,6 @@ namespace BookListRazor.Model
         [Required]
         public string Name { get; set; }
         public string Author { get; set; }
+        public string ISBN { get; set; }
     }
 }
