a8676e1ce93ca03d986bfef108dd6473e4c37b52
ritzyRosabel <rosabelolugbenga@yahoo.com>
2020-07-11 23:55:43 +0100

    changed width to fit browser window

--- BookListRazor/wwwroot/js/BookListJavaScript.js
+++ BookListRazor/wwwroot/js/BookListJavaScript.js
@@ -12,17 +12,18 @@ function loadDataTable() {
             "datatype":"json"
         },
         "columns": [
-            {"data":"name", "width":"30%"},
-            {"data":"author", "width":"30%"},
-            { "data": "isbn", "width": "30%" },
+            {"data":"name", "width":"20%"},
+            {"data":"author", "width":"20%"},
+            { "data": "isbn", "width": "20%" },
             {
                 "data": "id",
                 "render": function (data) {
                     return `<div>
-                        <a href = "/BookList/Edit?id=${data}" class = 'btn btn-success text-white' style = 'cursor : pointer; width : 100px;'>Edit</a>
-                        <a class ='btn btn-danger text-white' style = 'cursor:pointer; width:100px'> Delete</a>
+                        <a href = "/BookList/Edit?id=${data}" class = 'btn btn-success text-white' style = 'cursor : pointer; width : 70px;'>Edit</a>
+                        <a class ='btn btn-danger text-white' style = 'cursor:pointer; width:70px'> Delete</a>
                             </div>`
-                }
+                },
+                "width":"40%"
             }
         ],
         "language": {
