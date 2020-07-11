0fdd75d1b3cf46340fe8a18ad833ed9da9ac7a13
ritzyRosabel <rosabelolugbenga@yahoo.com>
2020-07-11 23:45:12 +0100

    edit button

--- BookListRazor/wwwroot/js/BookListJavaScript.js
+++ BookListRazor/wwwroot/js/BookListJavaScript.js
@@ -12,13 +12,15 @@ function loadDataTable() {
             "datatype":"json"
         },
         "columns": [
-            {"data":"name", "width":"30%"}
-            {"data":"author", "width":"30%"}
-            { "data": "isbn", "width": "30%" }
+            {"data":"name", "width":"30%"},
+            {"data":"author", "width":"30%"},
+            { "data": "isbn", "width": "30%" },
             {
                 "data": "id",
                 "render": function (data) {
-                   
+                    return `<div>
+                        <a href = "/BookList/Edit?id=${data}" class = 'btn btn-success text-white' style = 'cursor : pointer; width : 100px;'></a>
+                            </div>`
                 }
             }
         ]
