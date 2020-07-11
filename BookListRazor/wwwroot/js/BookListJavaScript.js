6eeb4fb4eab761aef7814a58010c97f336ce7126
ritzyRosabel <rosabelolugbenga@yahoo.com>
2020-07-11 23:39:16 +0100

    adding a render function

--- BookListRazor/wwwroot/js/BookListJavaScript.js
+++ BookListRazor/wwwroot/js/BookListJavaScript.js
@@ -1,7 +1,9 @@
 ﻿var dataTable;
+
 $(document).ready(function () {
     loadDataTable();
 });
+
 function loadDataTable() {
     dataTable = $('#DT_load').DataTable({
         "ajax": {
@@ -9,6 +11,16 @@ function loadDataTable() {
             "type": "GET",
             "datatype":"json"
         },
-        "columns"
+        "columns": [
+            {"data":"name", "width":"30%"}
+            {"data":"author", "width":"30%"}
+            { "data": "isbn", "width": "30%" }
+            {
+                "data": "id",
+                "render": function (data) {
+                   
+                }
+            }
+        ]
     });
 }
