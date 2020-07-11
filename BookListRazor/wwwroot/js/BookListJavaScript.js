bbbe902aec9d63d86905e7845f510cb5979857d9
ritzyRosabel <rosabelolugbenga@yahoo.com>
2020-07-11 23:28:14 +0100

    javascript file

---
+++ BookListRazor/wwwroot/js/BookListJavaScript.js
@@ -0,0 +1,14 @@
+﻿var dataTable;
+$(document).ready(function () {
+    loadDataTable();
+});
+function loadDataTable() {
+    dataTable = $('#DT_load').DataTable({
+        "ajax": {
+            "url": "/api/Books",
+            "type": "GET",
+            "datatype":"json"
+        },
+        "columns"
+    });
+}
